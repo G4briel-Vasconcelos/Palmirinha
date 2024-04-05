c1be791bde167e5cb281ef43ec53cb98bbb07822
gabriel Vasconcelos <gabriel49vasconcelos@gmail.com>
2024-04-05 15:10:04 -0300

    Corrigindo o erro de nao aparecer o nome da categoria

--- ReceitaApp/models.py
+++ ReceitaApp/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Categoria(models.Model):
     nome = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.nome
+
 class Receita(models.Model):
 
     DIFICULDADES = [
